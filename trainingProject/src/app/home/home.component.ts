e77d62919cb4d4853222725f0d46961e03931468
Bhoomika KL <bhoomika.kl@amadeus.com>
2024-08-30 15:32:02 +0530

    resolved some error

--- trainingProject/src/app/home/home.component.ts
+++ trainingProject/src/app/home/home.component.ts
@@ -1,46 +1,54 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder,FormGroup, Validators,FormControl } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  FormControl,
+} from '@angular/forms';
 import { homeService } from '../../assets/home.service';
 import { Router } from '@angular/router';
-
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent implements OnInit{
-  HomeForm !: FormGroup;
+export class HomeComponent implements OnInit {
+  HomeForm!: FormGroup;
 
   ngOnInit() {
     this.initializeForm();
-    
   }
   constructor(
-    private fb: FormBuilder,route:Router,private service:homeService
-) { }
+    private fb: FormBuilder,
+    route: Router,
+    private service: homeService
+  ) {}
 
-  initializeForm(){
-    this.HomeForm = this.fb.group
-    ({
-      firstName:['',[Validators.required,Validators.pattern('^[a-zA-Z]+$')]],
-      lastName:['',[Validators.required,Validators.pattern('^[a-zA-Z]+$')]],
-      age:['',[Validators.required, Validators.pattern('^[0-9]*$'), Validators.min(1)]],
-      gender:['',Validators.required]
-
+  initializeForm() {
+    this.HomeForm = this.fb.group({
+      firstName: ['', [Validators.required, Validators.pattern('^[a-zA-Z]+$')]],
+      lastName: ['', [Validators.required, Validators.pattern('[a-zA-Z]+')]],
+      age: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern('^[0-9]*$'),
+          Validators.min(1),
+        ],
+      ],
+      gender: ['', Validators.required],
     });
   }
-  onSubmit(){
-     Object.keys(this.HomeForm.controls).forEach(controlName=>{
-      this.HomeForm.get(controlName)?.markAllAsTouched();
-     });
+  onSubmit() {
+    // Object.keys(this.HomeForm.controls).forEach((controlName) => {
+    //   this.HomeForm.get(controlName)?.markAllAsTouched();
+    // });
 
-    if(this.HomeForm.valid){
-      const user= this.HomeForm.value;
+    if (this.HomeForm.valid) {
+      const user = this.HomeForm.value;
       this.service.addDetailes(user);
-      console.log("user detailes",user)
+      console.log('user detailes', user);
     }
-
   }
-     
 }
